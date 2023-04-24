a1a8538edae9da89ab52248eb5f89ae32c3a1f37
Sarah Salmean <sarahsalmean@hotmail.com>
2023-04-24 18:00:36 +0200

    created react app

---
+++ app-project/src/components/app/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          OUR HOME PAGE!!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
