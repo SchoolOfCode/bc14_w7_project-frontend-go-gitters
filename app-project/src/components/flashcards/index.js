29d1b73320e500b413597d73039b595d7f4d7468
Sarah Salmean <sarahsalmean@hotmail.com>
2023-04-25 15:13:49 +0200

    Updated carousel

--- app-project/src/components/flashcards/index.js
+++ app-project/src/components/flashcards/index.js
@@ -7,13 +7,16 @@ export default function CardCarousel(props)
 {
     var items = [
         {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
+            name: "JSX",
+            description: "JSX stands for JavaScript XML, and it is a syntax extension of JavaScript that allows developers to write HTML-like code in their JavaScript files. JSX makes it easier to create and manipulate the user interface of a React application."
         },
         {
-            name: "Random Name #2",
-            description: "Hello World!"
-        }
+            name: "States",
+            description: "State is a JavaScript object that stores data and determines how a component renders and behaves. When the state of a component changes, React will automatically update the component to reflect those changes. State is an essential concept in React because it enables developers to create dynamic and interactive user interfaces",
+        },
+        {name: "Array properties",
+
+        description: "Array elements are object properties in the same way that toString is a property (to be specific, however, toString() is a method). Nevertheless, trying to access an element of an array as follows throws a syntax error because the property name is not valid:"}
     ]
 
     return (
@@ -35,9 +38,6 @@ export default function CardCarousel(props)
             <Paper style={{width: "500px"}} class="card">
                 <h2>{props.item.name}</h2>
                 <p>{props.item.description}</p>
-                <Button className="CheckButton">
-                    Check it out!
-                </Button>
             </Paper>
         )
     }
