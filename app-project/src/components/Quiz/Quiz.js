e14e25de0786d03108a55a959a0bb2489b1bdc48
samisile <samisile97@gmail.com>
2023-04-25 12:29:15 +0100

    Hard coded the mcq's

---
+++ app-project/src/components/Quiz/Quiz.js
@@ -0,0 +1,6 @@
+// function Quiz() {
+
+
+// }
+
+// export Quiz as defualt 
--- app-project/src/components/app/App.js
+++ app-project/src/components/app/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import './App.css';
+import MultipleChoiceQ from "../Quiz/MCQs";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <p>
           OUR HOME PAGE!!!
         </p>
+        <MultipleChoiceQ />
       </header>
     </div>
   );
