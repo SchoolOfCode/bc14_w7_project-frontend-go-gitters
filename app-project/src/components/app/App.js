a357825022d5dc0498f23770de5f9c836ec3ecad
samisile <samisile97@gmail.com>
2023-04-26 15:41:07 +0100

    created pages for each of our components

--- app-project/src/components/app/App.js
+++ app-project/src/components/app/App.js
@@ -1,21 +1,37 @@
 import React from "react"
-import CardCarousel from "../flashcards";
+import FlashCardPage from "../../Pages/FlashCardPage";
 import './App.css';
 import "../flashcards/index.css"
-import Navbar from "../nav/nav"
-import Quiz from "../Quiz/Quiz";
+// import Navbar from "../nav/nav"
+import QuizPage from "../../Pages/QuizPage";
+import Home from "../../Pages/Home";
+import { BrowserRouter as Route, Routes, BrowserRouter } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <Navbar></Navbar>
-        <Quiz />
-      </header>
-      <div className= "div1"><CardCarousel/></div>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}  />
+          <Route path="../../Pages/QuizPage.js" element={<QuizPage />} />
+          <Route path="../../Pages/FlashCardPage" element={<FlashCardPage />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route path="/about" component={About} />
+//           <Route path="/contact" component={Contact} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
