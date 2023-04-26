8714051c8ca365a72c784fc969daf61ea6e4dbe1
samisile <samisile97@gmail.com>
2023-04-26 16:29:12 +0100

    semi working router

--- app-project/src/Pages/Home.js
+++ app-project/src/Pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div>
       <h1>Welcome to the Home page!</h1>
-      <Link to="../Quiz/Quiz.js">Test</Link>
-      <Link to="../nav/nav.js">Learn</Link>
+      {/* <Link to="../Quiz/Quiz.js">Test</Link>
+      <Link to="../nav/nav.js">Learn</Link> */}
     </div>
   );
 }
