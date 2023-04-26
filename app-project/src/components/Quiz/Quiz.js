59a118313957b94c46dd13a93ef400b2033af5f8
samisile <samisile97@gmail.com>
2023-04-26 11:19:05 +0100

    attempting to sort the quiz logic

--- app-project/src/components/Quiz/Quiz.js
+++ app-project/src/components/Quiz/Quiz.js
@@ -10,17 +10,16 @@ import "./Quiz.css"
 
 
 function Quiz() {
-    const [currentQuestion, setCurrentQuestion] = useState(0);
 
     const questions = [
        {
         question: "What is a React component?", 
         questionNumber: "1",
         answerOptions: [
-            {answerText: "A visual element of a web page"},
-            {answerText: "A unit of code that can be reused"},
-            {answerText: "A browser extension"},
-            {answerText: "A type of CSS selector"},
+            {answerText: "A visual element of a web page", answer: false},
+            {answerText: "A unit of code that can be reused", answer: true},
+            {answerText: "A browser extension", answer: false},
+            {answerText: "A type of CSS selector", answer: false },
         ]
        },
 
@@ -28,10 +27,10 @@ function Quiz() {
         question: "What is JSX?",
         questionNumber: "2",
         answerOptions: [
-            {answerText: "A file format for images"},
-            {answerText: "A JavaScript library for manipulating HTML"},
-            {answerText: "A programming language"},
-            {answerText: "A syntax extension for JavaScript"},
+            {answerText: "A file format for images", answer: false},
+            {answerText: "A JavaScript library for manipulating HTML", answer: false},
+            {answerText: "A programming language", answer: false},
+            {answerText: "A syntax extension for JavaScript", answer: true},
         ]
        }, 
 
@@ -39,90 +38,103 @@ function Quiz() {
        question: "What is the purpose of a constructor in a React component?", 
        questionNumber: "3",
        answerOptions: [
-            {answerText: "To create the component's HTML structure"},
-            {answerText: "To initialize the component's state and bind event handlers"}, 
-            {answerText: "To set the component's CSS styles"},
-            {answerText: "To handle user input"},
+            {answerText: "To create the component's HTML structure", answer: false},
+            {answerText: "To initialize the component's state and bind event handlers", answer: true}, 
+            {answerText: "To set the component's CSS styles", answer: false},
+            {answerText: "To handle user input", answer: false},
        ]}, 
         
        {
         question: "How do you render a React component?",
         questionNumber: "4",
         answerOptions: [
-             {answerText: "By calling its render() method"},
-             {answerText: "By calling its constructor"}, 
-             {answerText: "By using the ReactDOM.render() method"},
-             {answerText: "By setting the component's state"},
+             {answerText: "By calling its render() method", answer: true},
+             {answerText: "By calling its constructor", answer: false}, 
+             {answerText: "By using the ReactDOM.render() method", answer: false},
+             {answerText: "By setting the component's state", answer: false},
         ]}, 
 
         {
         question: "What is the difference between props and state in a React component?", 
         questionNumber: "5", 
         answerOptions: [
-            {answerText: "Props are used to pass data to a component, while state is used to manage data within a component"}, 
-            {answerText: "Props and state are the same thing"}, 
-            {answerText: "Props are used for styling, while state is used for data management"}, 
-            {answerText: "State is used to pass data to a component, while props are used to manage data within a component"}, 
+            {answerText: "Props are used to pass data to a component, while state is used to manage data within a component", answer: true}, 
+            {answerText: "Props and state are the same thing", answer: false}, 
+            {answerText: "Props are used for styling, while state is used for data management", answer: false}, 
+            {answerText: "State is used to pass data to a component, while props are used to manage data within a component", answer: false}, 
     ]}, 
 
         {
         question: "What is the purpose of the render() method in a React component?", 
         questionNumber: "6", 
         answerOptions: [
-            {answerText: "To update the component's state"}, 
-            {answerText: "To render the component's HTML structure"}, 
-            {answerText: "To bind event handlers to the component"}, 
-            {answerText: "To pass data to child components"}, 
+            {answerText: "To update the component's state", answer: false}, 
+            {answerText: "To render the component's HTML structure", answer: true}, 
+            {answerText: "To bind event handlers to the component", answer: false}, 
+            {answerText: "To pass data to child components", answer: false}, 
         ]}, 
     
         {
         question: "How do you define a functional component in React?", 
         questionNumber: "7", 
         answerOptions: [
-            {answerText: "By extending the Component class"}, 
-            {answerText: "By using the function keyword"}, 
-            {answerText: "By using the render() method"}, 
-            {answerText: "By creating a new instance of the React.Component class"}, 
+            {answerText: "By extending the Component class", answer: false}, 
+            {answerText: "By using the function keyword", answer: true}, 
+            {answerText: "By using the render() method", answer: false}, 
+            {answerText: "By creating a new instance of the React.Component class", answer: false}, 
     ]}, 
 
         {
         question: "What is the purpose of the key prop in a React component?", 
         questionNumber: "8", 
         answerOptions: [
-            {answerText: "To uniquely identify a component in a list"}, 
-            {answerText: "To set the component's CSS styles"}, 
-            {answerText: "To define the component's event handlers"}, 
-            {answerText: "To define the component's props"}, 
+            {answerText: "To uniquely identify a component in a list", answer: false}, 
+            {answerText: "To set the component's CSS styles", answer: false}, 
+            {answerText: "To define the component's event handlers", answer: false}, 
+            {answerText: "To define the component's props", answer: true}, 
     ]},
 
         {
         question: "How do you pass data from a parent component to a child component in React?", 
         questionNumber: "9",
         answerOptions: [
-            {answerText: "By using props"}, 
-            {answerText: "By using state"}, 
-            {answerText: "By using the constructor"}, 
-            {answerText: "By using the render() method"}, 
+            {answerText: "By using props", answer: true}, 
+            {answerText: "By using state", answer: false}, 
+            {answerText: "By using the constructor", answer: false}, 
+            {answerText: "By using the render() method", answer: false}, 
     ]},
     
         {
         question: "How do you create a new instance of a React component?", 
         questionNumber: "10",
         answerOptions: [
-            {answerText: "By using the React.createClass() method"}, 
-            {answerText: "By calling the component's constructor"}, 
-            {answerText: "By using the ReactDOM.render() method"}, 
-            {answerText: "By using the new keyword"}, 
+            {answerText: "By using the React.createClass() method", answer: false}, 
+            {answerText: "By calling the component's constructor", answer: false}, 
+            {answerText: "By using the ReactDOM.render() method", answer: true}, 
+            {answerText: "By using the new keyword", answer: false}, 
     ]},
-    
     ]
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
 
     const nextButtonClick = () => {
         const nextQuestion = currentQuestion + 1;
         setCurrentQuestion(nextQuestion);
     };
+    
+    // const answerChecker = (selectedAnswer, index) => {
+    //         setSelectedAnswer(index);
+    //         if (selectedAnswer.answer === true) {
+    //             console.log("correct");
+    //         } else {
+    //             console.log("incorrect");
+    //         }
+    //     };
 
-    let multiChoiceOptions = questions[currentQuestion].answerOptions.map((answerOption) =>  <li onClick={nextButtonClick}>{answerOption.answerText}</li> );
+    let multiChoiceAnswer = questions[currentQuestion].answerOptions.map((answerOption) =>  <li>{answerOption.answer}</li> );
+       
+    let multiChoiceOptions = questions[currentQuestion].answerOptions.map((answerOption) =>  <li>{answerOption.answerText}</li> );
        
 
     return(
@@ -132,6 +144,7 @@ function Quiz() {
         <p>{ questions[currentQuestion].question }</p> 
             <ul>
                 {multiChoiceOptions}
+                {multiChoiceAnswer}
             </ul>
             <button onClick={nextButtonClick}>Next</button>
         </div>
