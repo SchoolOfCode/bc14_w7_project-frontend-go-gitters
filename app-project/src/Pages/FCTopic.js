b90c5b9f3c11aec305b50e4d2f2568720a364fd0
Sarah Salmean <sarahsalmean@hotmail.com>
2023-04-27 19:11:25 +0200

    tidied resource page, updated fonts, menu color changed, titles added to pages except quiz page which I couldn't get to work

--- app-project/src/Pages/FCTopic.js
+++ app-project/src/Pages/FCTopic.js
@@ -5,9 +5,11 @@ export default function FCTopic() {
 
     return (
     <div>
-    <h2 className = "heading">Study Topic:</h2>
+    <img></img>
+    <h2 className = "heading">Flashcard Bank</h2>
+    <h2 className ="description">Whether you're confused by CSS, angry at arrays, or completely over objects, we have the flashcards for you.<br></br><br></br>Select a topic below, or click 'susprise me' for a random deck of cards.</h2>
     <div className = "resources-display">
-        <div className= "topic"><a href="../Learn/Flashcards">All Topics </a></div>
+        <div className= "topic"><a href="../Learn/Flashcards">Surprise me! </a></div>
         <div className= "topic">Objects</div>
         <div className= "topic">APIs</div>
         <div className= "topic">UX</div>
@@ -23,9 +25,16 @@ export default function FCTopic() {
     <div className = "resources-display">
         <div className= "topic">If statements</div>
         <div className= "topic">The DOM</div>
-        <div className= "topic">Javascript</div>
+        <div className= "topic">States</div>
         <div className= "topic">React</div>
         <div className= "topic">React Hooks</div>
+    </div>
+    <div className = "resources-display">
+        <div className= "topic">Testing</div>
+        <div className= "topic">UI libraries</div>
+        <div className= "topic">Advanced HTML</div>
+        <div className= "topic">Logic</div>
+        <div className= "topic">AI</div>
     </div>
     </div>
     )
--- app-project/src/Pages/Home.js
+++ app-project/src/Pages/Home.js
@@ -9,13 +9,13 @@ function Home() {
     <div className = "HomePage">
     <div className = "text">
     <h1 className='main-heading'>Welcome Back!</h1>
-    <h3>Welcome to our home webpage for studying code!
+    <h3>Master coding from bootcamp and beyond with the help of our library of quizzes, flashcards and resources.
 
-We believe that coding is one of the most important skills for the 21st century, and we're committed to helping you learn and master it.</h3>
+Coding is one of the most important skills for the 21st century, and we're committed to making sure that you master it.</h3>
 
-<h3>Whether you're a complete beginner or an experienced developer looking to improve your skills, we have something for you.
+<h3>Whether you're a beginner, bootcamper, or boss, we have something for you.
 
-Join our community of learners and start your coding journey today!</h3> 
+Become a Code Master today!</h3> 
 <div class="buttonDiv">
 <a href="../Test"><button className="button-56">Test</button> </a>
 <a href="../Learn"><button className="button-56">Learn</button></a> </div>
